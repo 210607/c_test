f092d26a16ce6399a1ed02fee2d1dce9b9c012e3
210607 <85469887+210607@users.noreply.github.com>
2024-02-13 22:40:57 +0800

    Add files via upload

---
+++ code_example/test.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    printf("你好！\n");
+    printf("hehe！\n");
+    printf("%zu\n", sizeof(char));
+    printf("%zu\n", sizeof(short));
+    printf("%zu\n", sizeof(int));
+    printf("%zu\n", sizeof(long));
+    printf("%zu\n", sizeof(long long));
+    printf("%zu\n", sizeof(float));
+    printf("%zu\n", sizeof(double));
+    return 0;
+}
